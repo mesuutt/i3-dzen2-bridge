c4c46e109900af516daecbd4296ee86e965b7419
Mesut Tasci <slackermesut@gmail.com>
2016-04-30 02:51:36 +0300

    Pass i3dzen2.sh paremeters to i3status

--- i3dzen2.sh
+++ i3dzen2.sh
@@ -16,7 +16,7 @@ PROJECT_PATH=`dirname "$SELF_PATH"`
 cd "$PROJECT_PATH"
 
 sleep 3
-i3status | ./i3-dzen-bridge.py | dzen2 -y -1 -fn "ubuntu mono" -ta r -dock &
+i3status $@ | ./i3-dzen-bridge.py | dzen2 -y -1 -fn "ubuntu mono" -ta r -dock &
 
 exit 0
 
